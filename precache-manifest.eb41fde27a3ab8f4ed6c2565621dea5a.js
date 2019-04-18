240bb8a1215c6c409b68bb2b9f836607eccd6213
massart <f_massart@hotmail.com>
2019-04-18 10:00:23 +0200

    Updates

---
+++ precache-manifest.eb41fde27a3ab8f4ed6c2565621dea5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/happydrink-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c88021aaef0636df78cc",
+    "url": "/happydrink-app/static/js/runtime~main.3f3db196.js"
+  },
+  {
+    "revision": "d94c942890fca49bc1c8",
+    "url": "/happydrink-app/static/js/main.76c4e757.chunk.js"
+  },
+  {
+    "revision": "eb2a579798c39c0dd5b6",
+    "url": "/happydrink-app/static/js/2.06d12eb5.chunk.js"
+  },
+  {
+    "revision": "d94c942890fca49bc1c8",
+    "url": "/happydrink-app/static/css/main.36af7f25.chunk.css"
+  },
+  {
+    "revision": "c5c2c5e47710ca31cc23653a7df32fcf",
+    "url": "/happydrink-app/index.html"
+  }
+];
